c9fc761b620fb44a86e52683af8038b841f43f06
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-01 19:58:30 +0200

    Edited views

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,4 +1,17 @@
-<div>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Product List</title>
+</head>
+<body>
+
+<div class="m-5">
+    <div class="mb-5 text-right">
+        <a href="{{ route('products.create') }}" class="btn btn-primary btn-sm">
+            Create new product
+        </a>
+    </div>
     <table class="table">
         <thead>
         <tr>
@@ -6,6 +19,7 @@
             <th scope="col">Product</th>
             <th scope="col">Type</th>
             <th scope="col">Price</th>
+            <th scope="col"></th>
         </tr>
         </thead>
         <tbody>
@@ -18,9 +32,24 @@
                     </a>
                 </td>
                 <td>{{ $product->type }}</td>
-                <td>{{ $product->price }}</td>
+                <td>€ {{ $product->getPrice() }}</td>
+                <td>
+                    <a href="{{ route('products.edit', $product) }}" class="btn btn-sm btn-warning">
+                        Edit
+                    </a>
+                    <form method="post" action="{{ route('products.destroy', $product) }}" style="display: inline-block">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-sm btn-danger" onclick="return confirm('Are you sure?')">
+                            Delete
+                        </button>
+                    </form>
+                </td>
             </tr>
         @endforeach
         </tbody>
     </table>
 </div>
+
+</body>
+</html>
--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,19 +1,50 @@
-<div>
-    <div>
-        Product: {{ $product->name }}
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>{{ $product->name }}</title>
+</head>
+
+<body>
+    <div class="m-5">
+        <div class="mb-5">
+            <a href="{{ route('products.index') }}" class="btn btn-primary btn-sm">
+                Back to product list
+            </a>
+        </div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="text-center mb-5">
+                    <h1>Product Description</h1>
+                </div>
+                <div>
+                    <h5><strong>Product: </strong>{{ $product->name }}</h5>
+                </div>
+                <div>
+                    <h5><strong>Product description: </strong>{{ $product->type }}</h5>
+                </div>
+                <div>
+                    <h5><strong>Product price: </strong>€ {{ $product->getPrice() }}</h5>
+                </div>
+                <div>
+                    <h5><strong>Product size: </strong>{{ $product->size }}</h5>
+                </div>
+                <div class="text-center mt-5 mb-5">
+                    <h1>Delivery methods and prices</h1>
+                </div>
+
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        <div class="text-center">
+            <ul class="list-unstyled">
+                @foreach($product->deliveries as $delivery)
+                    <li><h5><strong>{{ $delivery->name}}</strong> - {{ $delivery->getPrice() }}</h5></li>
+                @endforeach
+            </ul>
+        </div>
     </div>
-    <div>
-        Product description: {{ $product->type }}
-    </div>
-    <div>
-        Product price: {{ $product->price }}
-    </div>
-    <div>
-        Product size: {{ $product->size }}
-    </div>
-    <ul>
-        @foreach($product->deliveries as $delivery)
-            <li>{{ $delivery->name}}  {{ $delivery->price }}</li>
-        @endforeach
-    </ul>
-</div>
+</body>
+
+</html>
