afd39e2e9a63a81745b600663404a897e198878d
Juris Petrovs <jura-petrov@inbox.lv>
2020-12-01 19:59:47 +0200

    Added new methods

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -16,12 +16,17 @@ class ProductsController extends Controller
 
     public function create()
     {
-
+        return view('products.create');
     }
 
-    public function store()
+    public function store(Request $request)
     {
+        $product = (new Product())->fill($request->all());
+        $product->price *= 100;
 
+        $product->save();
+
+        return redirect()->route('products.index');
     }
 
     public function show(Product $product)
@@ -33,18 +38,26 @@ class ProductsController extends Controller
         ]);
     }
 
-    public function edit()
+    public function edit(Product $product)
     {
-
+        return view('products.edit', [
+            'product' => $product
+        ]);
     }
 
-    public function update()
+    public function update(Request $request, Product $product)
     {
+        $request['price'] *= 100;
 
+        $product->update($request->all());
+
+        return redirect()->route('products.edit', $product);
     }
 
-    public function destroy()
+    public function destroy(Product $product)
     {
+        $product->delete();
 
+        return redirect()->route('products.index');
     }
 }
